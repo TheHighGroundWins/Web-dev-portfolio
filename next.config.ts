782e5efa50e5ce04bfe5a7420f29350a3c0f963f
juan-carlos <carlosalberto.j@northeastern.edu>
2025-07-16 23:22:52 +0800

    changes

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: "/Web-dev-portfolio",
 };
 
 export default nextConfig;
